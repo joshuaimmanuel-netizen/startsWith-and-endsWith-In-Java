8513d41bfda36121d44b69f3ab1ca9c366c88df0
joshuaimmanuel-netizen <106328263+joshuaimmanuel-netizen@users.noreply.github.com>
2022-07-31 23:36:53 +0530

    Add files via upload
    .

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String[] args){
+        String a="joshua immanuel";
+        String b="java program";
+        System.out.println(a.startsWith("joshua"));
+        System.out.println(a.endsWith("immanuel"));
+    }
+}
